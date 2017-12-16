d668a3daf8d26889c0eba00eeb8ab53c70c71196
Michael Odumosu <michaelodumosu57@gmail.com>
2017-12-15 19:48:49 -0500

    xml and php testing

--- sendEmail.php
+++ sendEmail.php
@@ -1,7 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
 $mailTo = $_POST['emailTo'];
 $mailFrom = $_POST['emailFrom'];
